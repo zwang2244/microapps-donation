3f4fd8dcbf19d1f05f0f373b951370bf0e6f7529
zwang2244 <zwang2244@wisc.edu>
2021-10-11 17:18:49 -0500

    Add test for payment widget

--- selenium_tests.py
+++ selenium_tests.py
@@ -8,7 +8,9 @@ from selenium.webdriver.support.ui import Select
 
 class MircoAppDonationTests(unittest.TestCase):
     def setUp(self):
-        self.driver = webdriver.Chrome("drivers/chromedriver")
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument("--disable-popup-blocking")
+        self.driver = webdriver.Chrome("drivers/chromedriver", chrome_options=chrome_options)
 
     def test_donate_with_USD(self):
         driver = self.driver
@@ -74,6 +76,33 @@ class MircoAppDonationTests(unittest.TestCase):
         alert = driver.find_element_by_css_selector('.alert.alert-danger.erroralert')
         assert alert.text == "Oh snap! Amount must be number!"
 
+
+    def test_the_payment_widget(self):
+        driver = self.driver
+        currency = 'USD'
+        amount = 0
+        driver.get("http://localhost:8080/")
+
+        # choose option
+        select = Select(driver.find_element_by_id('currency'))
+        select.select_by_value(currency)
+
+        # fill in text field
+        input = driver.find_element_by_id('amount')
+        input.send_keys(amount)
+
+        # click button
+        driver.find_element_by_css_selector('.btn.btn-primary.btn-block').click()
+        time.sleep(10) # wait for the message loading
+        Select(driver.find_element_by_name('paymentBrand')).select_by_value("VISA")
+        driver.find_element_by_name('card.number').send_keys("4111 1111 1111 1111")
+        expdate = driver.find_element_by_xpath("//input[@aria-label='Expiry Date']")
+        expdate.send_keys("1230")
+        driver.find_element_by_name('card.holder').send_keys("Test Person")
+        driver.find_element_by_name('card.cvv').send_keys("123")
+        driver.find_element_by_xpath("//button[@aria-label='Pay now']").click()
+        time.sleep(20)
+        print(driver.switch_to_alert().text)
 
     def tearDown(self):
         self.driver.close()
